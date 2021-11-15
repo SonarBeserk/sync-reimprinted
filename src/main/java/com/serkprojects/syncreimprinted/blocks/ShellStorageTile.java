a629cca047d65d7dddc78f61b621ffd64c995b2c
SonarBeserk <sonarbeserk@gmail.com>
2021-11-14 23:47:39 -0500

    Basic energy implementation for shell storage tiles

--- src/main/java/com/serkprojects/syncreimprinted/blocks/ShellStorageTile.java
+++ src/main/java/com/serkprojects/syncreimprinted/blocks/ShellStorageTile.java
@@ -9,18 +9,27 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class ShellStorageTile extends TileEntity implements ITickableTileEntity {
+public class ShellStorageTile extends TileEntity implements ITickableTileEntity, IEnergyStorage {
+    private boolean top;
+    private ShellStorageTile PairedBlock;
+
     private boolean occupied;
     private boolean updatedVisuals;
 
+    private int currentEnergy;
+    private int maxEnergy;
+
     public ShellStorageTile() {
         super(Registration.SHELLSTORAGE_TILE.get());
+        currentEnergy = 0;
+        maxEnergy = 10000;
     }
 
     @Override
@@ -49,6 +58,8 @@ public class ShellStorageTile extends TileEntity implements ITickableTileEntity 
     @Override
     public void read(BlockState state, CompoundNBT tag) {
         occupied = tag.getBoolean("occupied");
+        currentEnergy = tag.getInt("currentenergy");
+        maxEnergy = tag.getInt("maxenergy");
         super.read(state, tag);
     }
 
@@ -56,6 +67,38 @@ public class ShellStorageTile extends TileEntity implements ITickableTileEntity 
     public CompoundNBT write(CompoundNBT tag) {
         tag = super.write(tag);
         tag.putBoolean("occupied", occupied);
+        tag.putInt("currentenergy", currentEnergy);
+        tag.putInt("maxenergy", maxEnergy);
         return tag;
+    }
+
+    @Override
+    public int receiveEnergy(int maxReceive, boolean simulate) {
+        return 0;
+    }
+
+    @Override
+    public int extractEnergy(int maxExtract, boolean simulate) {
+        return 0;
+    }
+
+    @Override
+    public int getEnergyStored() {
+        return !top ? currentEnergy : 0;
+    }
+
+    @Override
+    public int getMaxEnergyStored() {
+        return !top ? maxEnergy : 0;
+    }
+
+    @Override
+    public boolean canExtract() {
+        return false;
+    }
+
+    @Override
+    public boolean canReceive() {
+        return !top;
     }
 }
